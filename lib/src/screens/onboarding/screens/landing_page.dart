a564e3e06ed9239977023e5f103f50e6aa0bc0d6
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-05-05 20:55:37 +0300

    init, debug, lib recreation

---
+++ lib/src/screens/onboarding/screens/landing_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lite_cart/src/screens/onboarding/ui_view/slider_layout_view.dart';
+
+
+class LandingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: onBordingBody(),
+    );
+  }
+
+  Widget onBordingBody() => Container(
+        child: SliderLayoutView(),
+      );
+}
